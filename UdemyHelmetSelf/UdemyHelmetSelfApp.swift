3c2eee33d19bce119dd855fca161c732966dbbba
Kyungyun Lee, IOS dev <leeky16498@gmail.com>
2022-03-11 00:25:23 +0000

    Initial Commit

---
+++ UdemyHelmetSelf/UdemyHelmetSelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdemyHelmetSelfApp.swift
+//  UdemyHelmetSelf
+//
+//  Created by Kyungyun Lee on 11/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct UdemyHelmetSelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
